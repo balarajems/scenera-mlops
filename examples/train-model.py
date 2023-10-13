236bec57886d6fa72f7fcfed8c98e89c9d93b750
Teresa Crane <teresacrane@microsoft.com>
2023-10-13 02:58:25 -0400

    ML Flow

--- examples/train-model.py
+++ examples/train-model.py
@@ -1,75 +1,34 @@
-import http.client, urllib.request, urllib.parse, urllib.error, requests
+import requests
+import json
+import os
+import mlflow
 
+cv_key = os.getenv('AZURE_VISION_KEY')
 headers = {
     # Request headers
     'Content-Type': 'application/json-patch+json',
-    'Ocp-Apim-Subscription-Key': '2c1491d2155e44d795c502005873182b',
+    'Ocp-Apim-Subscription-Key': cv_key,
 }
 
-params = urllib.parse.urlencode({})
-data = {
-  "name": "apiModel",
-  "createdDateTime": "2023-10-11T06:01:57.192Z",
-  "updatedDateTime": "2023-10-11T06:01:57.192Z",
-  "status": "notStarted",
-  "trainingParameters": {
-    "trainingDatasetName": "testcocodataset",
-    "timeBudgetInHours": 1,
-    "modelKind": "Generic-Detector"
-  },
-  "trainingCostInMinutes": 0,
-  "error": {
-    "code": "string",
-    "message": "string",
-    "target": "string",
-    "details": [
-      "string"
-    ],
-    "innererror": {
-      "code": "string",
-      "message": "string",
-      "innererror": "string"
-    }
-  },
-  "modelPerformance": {
-    "accuracyTop1": 0,
-    "accuracyTop5": 0,
-    "averagePrecision": 0,
-    "calibrationECE": 0,
-    "meanAveragePrecision30": 0,
-    "meanAveragePrecision50": 0,
-    "meanAveragePrecision75": 0,
-    "tagPerformance": {
-      "additionalProp1": {
-        "accuracy": 0,
-        "averagePrecision50": 0
-      },
-      "additionalProp2": {
-        "accuracy": 0,
-        "averagePrecision50": 0
-      },
-      "additionalProp3": {
-        "accuracy": 0,
-        "averagePrecision50": 0
-      }
-    }
-  }
-}
-
-endpoint = "https://scenera-computervision-rnd-resource.cognitiveservices.azure.com"
-model_name = "scenera-custom-model-v1"
+endpoint = "https://cv-teresacrane-demo.cognitiveservices.azure.com/"
+model_name = "sample-model"
+mlflow.set_tracking_uri("azureml://eastus.api.azureml.ms/mlflow/v1.0/subscriptions/a77f25b0-1202-42cf-b41b-f72ea97f44fc/resourceGroups/scenera-demo/providers/Microsoft.MachineLearningServices/workspaces/aml-without-vnet")
 
 try:
-    # conn = http.client.HTTPSConnection('scenera-computervision-rnd-resource.cognitiveservices.azure.com')
-    # conn.request("PUT", "/computervision/models/scenera-custom-model-v1?api-version=2023-02-01-preview&%s" % params, "{}", headers)
-    # response = conn.getresponse()
-    # data = response.read()
-    # print(data)
-    # conn.close()
-    response = requests.put(f"{endpoint}/computervision/models/{model_name}?api-version=2023-02-01-preview", data=data, headers=headers)
+  with mlflow.start_run() as run:
+    response = requests.get(f"{endpoint}/computervision/models/{model_name}?api-version=2023-02-01-preview", headers=headers)
     print(response.status_code)
     print(response.content)
-    
+
+    metrics = json.loads(response.content)
+    mlflow.log_param("model_name", model_name)
+    mlflow.log_param("model_status", metrics["status"])
+    mlflow.log_param("model_training_params", metrics["trainingParameters"])
+    mlflow.log_metric("model_training_cost", metrics["trainingCostInMinutes"])
+    mlflow.log_metric("meanAveragePrecision30", metrics["modelPerformance"]["meanAveragePrecision30"])
+    mlflow.log_metric("meanAveragePrecision50", metrics["modelPerformance"]["meanAveragePrecision50"])
+    mlflow.log_metric("meanAveragePrecision75", metrics["modelPerformance"]["meanAveragePrecision75"])
+    print(f"run.info.run_id: {run.info.run_id} {run.info.run_name}")
 except Exception as e:
     print("[Errno {0}] {1}".format(e, e))
 
--- examples/train_model_v2.py
+++ examples/train_model_v2.py
@@ -1,19 +1,33 @@
-from cognitive_service_vision_model_customization_python_samples import TrainingClient, Model, ModelKind, TrainingParameters, EvaluationParameters, ResourceType
+from cognitive_service_vision_model_customization_python_samples import TrainingClient, ResourceType
 import os
+from mlflow import log_metric
 
 
-model_name = 'scenera_custom_model_v5'
-dataset_name = 'sceneradatasetv3'
-resource_name = "scenera-computervision-rnd-resource"
-multi_service_endpoint = None
 
-training_client = TrainingClient(ResourceType.SINGLE_SERVICE_RESOURCE, resource_name, multi_service_endpoint, os.getenv('RESOURCE_KEY'))
-train_params = TrainingParameters(training_dataset_name=dataset_name, time_budget_in_hours=1, model_kind=ModelKind.GENERIC_OD)  # checkout ModelKind for all valid model kinds
+model_name = 'sample-model'
+#dataset_name = 'sceneradatasetv3'
+resource_name = "cv-teresacrane-demo"
+#multi_service_endpoint = None
+#
+training_client = TrainingClient(ResourceType.SINGLE_SERVICE_RESOURCE, resource_name, any(), os.getenv('AZURE_VISION_KEY'))
+#train_params = TrainingParameters(training_dataset_name=dataset_name, time_budget_in_hours=1, model_kind=ModelKind.GENERIC_OD)  # checkout ModelKind for all valid model kinds
+#
+#eval_dataset = None
+#eval_params = EvaluationParameters(test_dataset_name=eval_dataset.name) if eval_dataset else None
+#
 
-eval_dataset = None
-eval_params = EvaluationParameters(test_dataset_name=eval_dataset.name) if eval_dataset else None
-
-
-model = Model(model_name, train_params, eval_params)
-model = training_client.train_model(model)
+model = training_client.query_model(model_name)
+#model = Model(model_name, train_params, eval_params)
+#model = training_client.train_model(model)
 print(f'Start training: {model.__dict__}')
+
+#TODO: Baba working on checking for the model results
+#mlflow.start_tracking_uri("http://localhost:5000")
+
+log_metric("model_name", model.name)
+log_metric("model_status", model.status)
+log_metric("model_training_params", model.training_params)
+log_metric("model_training_cost", model.training_cost)
+log_metric("model_performance", model.model_performance)
+
+
